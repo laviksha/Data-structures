3be23cc34967b68b009a0e9e7572d5798e229f42
laviksha soni <51523930+laviksha@users.noreply.github.com>
2021-06-05 20:33:04 +0530

    Create ConnectNodeAtSameLevelUsingQueue.java

---
+++ BinaryTree/ConnectNodeAtSameLevelUsingQueue.java
@@ -0,0 +1,19 @@
+ public void connect(Node root)
+    {
+       Queue<Node> q=new LinkedList<Node>();
+       q.add(root);
+       root.nextRight=null;
+       while(!q.isEmpty()){
+           int curSize=q.size();
+           
+           for(int i=0;i<curSize;i++){
+               Node cur=q.poll();
+               if(i==curSize-1)
+                cur.nextRight=null;
+               else 
+                 cur.nextRight=q.peek();
+               if(cur.left!=null) q.add(cur.left);
+               if(cur.right!=null) q.add(cur.right);
+           }
+       }
+    }
