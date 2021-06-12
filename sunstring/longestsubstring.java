3c3e7222446a26a9d91b919b04647a98889ea0b1
laviksha soni <51523930+laviksha@users.noreply.github.com>
2021-06-12 09:18:46 +0530

    Update longestsubstring.java

--- sunstring/longestsubstring.java
+++ sunstring/longestsubstring.java
@@ -1,21 +1,32 @@
  int longestUniqueSubsttr(String s){
-        String res=""+s.charAt(0);
-        LinkedHashSet<Character> t=new LinkedHashSet<Character>();
-        t.add(s.charAt(0));
+          public int lengthOfLongestSubstring(String s) {
+      //  String res=""+s.charAt(0);
+        if(s.length()==0) return 0;
+        LinkedHashMap<Character,Integer> t=new LinkedHashMap<Character,Integer>();
+        t.put(s.charAt(0),0);
         int maxTillNow=1;
         int curLength=1;
         for(int i=1;i<s.length();i++){
                         char cur=s.charAt(i);
-            if(t.contains(cur)){
+            if(t.containsKey(cur)){
                 if(maxTillNow<curLength) maxTillNow=curLength;
-                int lastOc=t.headSet(cur).size();
+                int lastOc=t.get(cur);
               // System.out.println(lastOc);
-                curLength-=(lastOc+1);
-                for(int j=0;j<=lastOc;j++){t.pollFirst();}
+                        Iterator hm = t.entrySet().iterator();
+                        HashSet<Character> temp=new HashSet<Character>();
+                while(hm.hasNext()){
+                    Map.Entry mapElement = (Map.Entry)hm.next();
+                    temp.add((Character)mapElement.getKey());
+                    if((Character)mapElement.getKey()==cur) break;
+                    lastOc--;
+                }
+                t.keySet().removeAll(temp);
+                    
+                curLength=t.size();
             }
-            t.add(cur);
+            t.put(cur,i);
             curLength++;
-          // System.out.println("cur:"+cur+" curlength"+curLength+" max:"+maxTillNow+" t:"+t);
+          //System.out.println("cur:"+cur+" curlength"+curLength+" max:"+maxTillNow+" t:"+t);
 
         }
         //for qwerty all length
