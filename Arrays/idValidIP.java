52b91252864b4cec8619ba3da140a8b51fc6aade
laviksha soni <51523930+laviksha@users.noreply.github.com>
2021-06-16 13:16:45 +0530

    Update idValidIP.java
    Write a program to Validate an IPv4 Address. According to Wikipedia, IPv4 addresses are canonically represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255, separated by dots, e.g., 172.16.254.1 . The generalized form of an IPv4 address is (0-255).(0-255).(0-255).(0-255). Here we are considering numbers only from 0 to 255 and any additional leading zeroes will be considered invalid.
    
    Your task is  to complete the function isValid which returns 1 if the ip address is valid else returns 0. The function takes a string s as its only argument .
    
    Example 1:
    
    Input:
    ip = 222.111.111.111
    Output: 1
    Example 2:
    
    Input:
    ip = 5555..555
    Output: 0

--- Arrays/idValidIP.java
+++ Arrays/idValidIP.java
@@ -1,23 +1,24 @@
-  public boolean isValid(String s) {
+ public boolean isValid(String s) {
         // Write your code here
         String[] splitted=s.split("\\.");
               //      System.out.println(splitted);
-int sumOfdigit=0;
-if(splitted.length!=4) return false;
+        int sumOfdigit=0;
+        if(splitted.length!=4) return false;
+        
         for(String s1:splitted){
            // System.out.println(s1);
-            if(s1.length()>3) return false;
+        if(s1.length()>3 || s1.equals("00") || s1.equals("000")) return false;
+          
           int x=0;
           try{
            x=Integer.parseInt(s1);}
           catch(Exception e){
               return false;
           }
-          if(x>255) return false;
-
-          sumOfdigit+=3;
+          if(x>255 || (x!=0 && s1.charAt(0)=='0')) return false;
+          sumOfdigit+=s1.length();
         }
+        if(s.length()!=(sumOfdigit+3)) return false;
         
         return true;
     }
-}
